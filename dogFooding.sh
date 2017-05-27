e9f79cc8acb0c36e65198b4a6f4caad2a7446638
Ryuichi Saito <ryuichi@ryuichisaito.com>
2017-05-27 14:21:15 -0500

    Register cyclomatic complexity and run dogfooding

--- dogFooding.sh
+++ dogFooding.sh
@@ -3,7 +3,18 @@
 set -e
 
 make
-for f in $(find . -regex ".*\.swift")
+
+for f in $(find . -regex "\.\/Sources.*\.swift")
 do
+  echo $f
   .build/debug/swift-lint $@ $f
 done
+
+for f in $(find . -regex "\.\/Tests.*\.swift")
+do
+  echo $f
+  .build/debug/swift-lint $@ $f
+done
+
+echo "Package.swift"
+.build/debug/swift-lint $@ Package.swift
