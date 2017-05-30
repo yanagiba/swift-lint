1a80ecfa5e3534e322968def50e99d9c365ca1ae
Ryuichi Saito <ryuichi@ryuichisaito.com>
2017-05-30 17:10:15 -0500

    Register rules

--- Tests/LintTests/DriverTests.swift
+++ Tests/LintTests/DriverTests.swift
@@ -67,7 +67,7 @@ class DriverTests : XCTestCase {
     let testDriver = Driver()
     testDriver.updateOutputHandle(.nullDevice)
     testDriver.setReporter(testDriverReporter)
-    testDriver.registerRule(TestDriverRule(), ruleIdentifiers: ["test_driver"])
+    testDriver.registerRules([TestDriverRule()], ruleIdentifiers: ["test_driver"])
     testDriver.lint(sourceFiles: [
       SourceFile(path: "test/testDriver", content: "import foo"),
     ])
@@ -80,7 +80,7 @@ class DriverTests : XCTestCase {
     let testDriver = Driver()
     testDriver.updateOutputHandle(.nullDevice)
     testDriver.setReporter(testDriverReporter)
-    testDriver.registerRule(TestDriverRule(), ruleIdentifiers: ["not_implemented"])
+    testDriver.registerRules([TestDriverRule()], ruleIdentifiers: ["not_implemented"])
     testDriver.lint(sourceFiles: [
       SourceFile(path: "test/testDriver", content: "import foo"),
     ])
