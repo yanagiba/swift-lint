c874d4f69af0f8abd55d27ed482055e892ab5f42
Ryuichi Saito <ryuichi@ryuichisaito.com>
2017-05-31 07:47:02 -0500

    Added description to cyclomatic complexity

--- Documentation/Rules/Complexity_JP.md
+++ Documentation/Rules/Complexity_JP.md
@@ -8,7 +8,9 @@
 
 分類: Complexity
 
+Cyclomatic complexity is determined by the number of linearly independent paths through a program's source code. In other words, cyclomatic complexity of a method is measured by the number of decision points, like `if`, `while`, and `for` statements, plus one for the method entry.
 
+The experiments McCabe, the author of cyclomatic complexity, conclude that methods in the 3 to 7 complexity range are quite well structured. He also suggest the cyclomatic complexity of 10 is a reasonable upper limit.
 
 
 
--- Sources/Lint/rule/CyclomaticComplexityRule.swift
+++ Sources/Lint/rule/CyclomaticComplexityRule.swift
@@ -26,7 +26,15 @@ class CyclomaticComplexityRule : RuleBase, ASTVisitorRule {
   static let DefaultThreshold = 10
 
   let name = "High Cyclomatic Complexity"
-  let description = ""
+  let description = "Cyclomatic complexity is determined by the number of " +
+    "linearly independent paths through a program's source code. " +
+    "In other words, cyclomatic complexity of a method is measured by " +
+    "the number of decision points, like `if`, `while`, and `for` statements, " +
+    "plus one for the method entry.\n\n" +
+    "The experiments McCabe, the author of cyclomatic complexity, " +
+    "conclude that methods in the 3 to 7 complexity range are " +
+    "quite well structured. He also suggest " +
+    "the cyclomatic complexity of 10 is a reasonable upper limit."
   let markdown = ""
   let severity = Issue.Severity.major
   let category = Issue.Category.complexity
